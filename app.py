3053f0f5134ee5fafd4a0b288bd686e230a9a7e2
hud0shnik <c4nsero@mail.ru>
2022-06-22 00:11:09 +0300

    update app.py
    Добавил комментарии
    Обновил конструктор для User
    Добавил проверки на 403 и 404
    Оптимизировал функцию find

--- app.py
+++ app.py
@@ -5,27 +5,57 @@ import requests
 
 app = Flask(__name__)
 
+# Пользователь
 class User:
-    def __init__(self, username, activity_status):
+
+    # Конструктор
+    def __init__(self, username, activity_status = ""):
         self.username = username
         self.activity_status = activity_status
-    
+
+    # Функция конвертирования в json
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4, ensure_ascii=False)
 
+# Ошибка
+class Error:
+    
+    # Конструктор
+    def __init__(self, title):
+        self.title = title
+
+    # Функция конвертирования в json
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4, ensure_ascii=False)
+
+# Функция поиска значений
 def find(fullStr, substr, stopChar) -> str:
-    i = fullStr.find(substr)
-    fullStr = fullStr[i+len(substr):]
+    fullStr = fullStr[fullStr.find(substr)+len(substr):]
     return fullStr[:fullStr.find(stopChar)]
 
+# Роут /user/
 @app.route('/user/<string:id>', methods=['GET'])
 def get_tasks(id):
+
+    # Объект, в который будет вестись запись
+    result = User(id)
+
+    # Получение html файла странички
     html = requests.get('https://vk.com/'+id).text
-    result = User(
-        id, 
-        find(html, '<span class="pp_last_activity_text">', '<')
-    )
+
+    # Проверка на существование пользователя
+    if html.find('<title>404 Not Found</title>') != -1:
+        return Error("404 Not Found").toJSON()
+
+    # Проверка на возможность обработки
+    if html.find('<div class="service_msg service_msg_null">') != -1:
+        return Error("403 Access Denied").toJSON()
+
+    # Статус активности
+    result.activity_status = find(html, '<span class="pp_last_activity_text">', '<')
+    
     return result.toJSON()
 
 if __name__ == '__main__':
