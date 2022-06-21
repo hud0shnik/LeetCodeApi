fce5317ddc13fa236b0b1f7189a325bc86dd483b
hud0shnik <c4nsero@mail.ru>
2022-06-21 23:30:46 +0300

    update app.py
    Теперь апиха возвращает статус пользователя (когда он был в сети)

--- app.py
+++ app.py
@@ -6,19 +6,26 @@ import requests
 app = Flask(__name__)
 
 class User:
-    def __init__(self, username):
+    def __init__(self, username, activity_status):
         self.username = username
+        self.activity_status = activity_status
     
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
+            sort_keys=True, indent=4, ensure_ascii=False)
+
+def find(fullStr, substr, stopChar) -> str:
+    i = fullStr.find(substr)
+    fullStr = fullStr[i+len(substr):]
+    return fullStr[:fullStr.find(stopChar)]
 
 @app.route('/user/<string:id>', methods=['GET'])
 def get_tasks(id):
-    result = User(id)
-    file = open("sample.html", "w")
-    file.write(requests.get('http://leetcode.com/'+id).text)
-    file.close()
+    html = requests.get('https://vk.com/'+id).text
+    result = User(
+        id, 
+        find(html, '<span class="pp_last_activity_text">', '<')
+    )
     return result.toJSON()
 
 if __name__ == '__main__':
